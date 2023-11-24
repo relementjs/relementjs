b40547c82bbed2a129d77e223db0991680a34713
Brian Takita <brian.takita@gmail.com>
2023-11-24 01:22:56 -0500

    minor:
    	+ render_api__use
    	- isomorphic__render__api_T
    	+ _
    	+ attach
    	add_T→attach_T
    	- browser__isomorphic__render__api_T
    	- server__isomorphic__render__api_T
    	+ bind_
    	+ tags
    	+ tags_T
    	+ tagsNS
    	+ html_
    	+ server__element__proto
    	+ hydrate

--- render/index.d.ts
+++ render/index.d.ts
@@ -1,42 +1,43 @@
-import { render__tags_env_T } from '@ctx-core/ui--any'
-import type { browser__add_T, browser__bind__T, browser__Tags } from '@ctx-core/ui--browser'
-import type { server__add_T, server__bind__T, server__Tags } from '@ctx-core/ui--server'
-import type { rmemo_T } from 'ctx-core/all'
-export declare function isomorphic__render__set<env_T extends render__env_T>(api:isomorphic__render__api_T<env_T>):void
+import type { bind__T, render__namespaceURI_T, render__tags_env_T, render_primitive_T } from '@ctx-core/ui--any'
+import type { browser__attach_T, browser__render_api_T, browser__Tags_T, rmemo_T } from '@ctx-core/ui--browser'
+import type {
+	server__attach_T,
+	server__element__proto_T,
+	server__html__T,
+	server__render_api_T,
+	server__Tags_T
+} from '@ctx-core/ui--server'
+export declare function render_api__use(
+	render_api:browser__render_api_T|server__render_api_T
+):void
 export type render__env_T = 'any'|'browser'|'server'
-export type isomorphic__render__api_T<env_T extends render__env_T> =
+export declare function _<T>(s:T|rmemo_T<T>):T
+export declare function attach<env_T extends render__env_T>(
+	...arg_a:Parameters<attach_T<env_T>>
+):ReturnType<attach_T<env_T>>
+export type attach_T<env_T extends render__env_T> =
 	env_T extends 'browser'
-		? browser__isomorphic__render__api_T
+		? browser__attach_T
 		: env_T extends 'server'
-			? server__isomorphic__render__api_T
-			: browser__isomorphic__render__api_T|server__isomorphic__render__api_T
-export type browser__isomorphic__render__api_T = {
-	_?:<T>(s:T|rmemo_T<T>)=>T
-	add?:browser__add_T
-	bind_?:browser__bind__T
-	tags?:browser__Tags<'html'>
-}
-export type server__isomorphic__render__api_T = {
-	_?:<T>(s:T|rmemo_T<T>)=>T
-	add?:server__add_T
-	bind_?:server__bind__T
-	tags?:server__Tags
-}
-export type add_T<env_T extends render__env_T> =
-	env_T extends 'browser'
-		? browser__add_T
-		: env_T extends 'server'
-			? server__add_T
-			: browser__add_T|server__add_T
-export type bind__T<env_T extends render__env_T> =
-	env_T extends 'browser'
-		? browser__bind__T
-		: env_T extends 'server'
-			? server__bind__T
-			: browser__bind__T|server__bind__T
+			? server__attach_T
+			: browser__attach_T|server__attach_T
+export declare function bind_():bind__T
+export declare let tags:tags_T<'any'>
 export type tags_T<env_T extends render__env_T, tags_env_T extends render__tags_env_T = 'html'> =
 	env_T extends 'browser'
-		? browser__Tags<tags_env_T>
+		? browser__Tags_T<tags_env_T>
 		: env_T extends 'server'
-			? server__Tags
-			: browser__Tags<tags_env_T>|server__Tags
+			? server__Tags_T
+			: browser__Tags_T<tags_env_T>|server__Tags_T
+export declare function tagsNS<env_T extends render__env_T, tags_env_T extends render__tags_env_T>(
+	namespaceURI:render__namespaceURI_T<tags_env_T>
+):tags_T<env_T, tags_env_T>
+export declare function html_<env_T extends render__env_T>():html__T<env_T>
+export type html__T<env_T extends render__env_T> =
+	env_T extends 'server'
+		? server__html__T
+		: env_T extends 'browser'
+			? never
+			: server__html__T
+export declare let server__element__proto:server__element__proto_T
+export declare function hydrate<T extends Node>(dom:T, f:(dom:T)=>T|null|undefined|render_primitive_T):void
--- render/index.js
+++ render/index.js
@@ -1,1 +1,21 @@
-export {}
+/** @typedef {import('@ctx-core/ui--browser').browser__render_api_T} */
+/** @typedef {import('@ctx-core/ui--server').server__render_api_T} */
+/** @type {browser__render_api_T|server__render_api_T} */
+export function render_api__use(render_api) {
+	_ = render_api?._
+	attach = render_api?.attach
+	bind_ = render_api?.bind_
+	tags = render_api?.tags
+	tagsNS = render_api?.tagsNS
+	html_ = render_api?.html_ || 'server-only'
+	server__element__proto = render_api?.server__element__proto || 'server-only'
+	hydrate = render_api?.hydrate || 'browser-only'
+}
+export let _
+export let attach
+export let bind_
+export let tags
+export let tagsNS
+export let html_
+export let server__element__proto
+export let hydrate
