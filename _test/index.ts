de9d1ee058912ee8ca073c1b1778ed613e56159e
Brian Takita <brian.takita@gmail.com>
2023-11-30 14:19:39 -0500

    tags: props: test: include angle brackets

--- _test/index.ts
+++ _test/index.ts
@@ -1,2 +1,3 @@
-export const prop_data__div_html = '<div data-foo="{&quot;foo_a&quot;:[&quot;\\&quot;bar\\&quot;&quot;,&quot;qux&quot;]}"></div>'
-export const prop_data__div_o = { foo_a: ['"bar"', 'qux' ] }
+export const prop_data__div_html = '<div' +
+	' data-foo="{&quot;foo_a&quot;:[&quot;<div>\\&quot;bar\\&quot;</div>&quot;,&quot;qux&quot;]}"></div>'
+export const prop_data__div_o = { foo_a: ['<div>"bar"</div>', 'qux' ] }
